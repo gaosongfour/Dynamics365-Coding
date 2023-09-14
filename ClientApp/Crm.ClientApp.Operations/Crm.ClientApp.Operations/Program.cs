90f630824b6e99d28bf7685e7a458091f8acba47
GaoSong11 <GaoSong11@12XR693>
2023-09-14 14:23:11 +0800

    #4 Create console application using CrmServiceClient, including the methods to retrieve current user security role and license info

---
+++ ClientApp/Crm.ClientApp.Operations/Crm.ClientApp.Operations/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Crm.ClientApp.Operations.UserOperations;
+
+namespace Crm.ClientApp.Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICrmOperation crmOperation = new UserSettingsOperation();
+            crmOperation.Execute();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ClientApp/Crm.ClientApp.Operations/Crm.Common.CrmServiceHelper/CrmConfigHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+using System.Text.Json;
+
+namespace Crm.Common.CrmService
+{
+    public class CrmConfigHelper
+    {
+        public static string GetCrmConnString(string configPath)
+        {
+            if (!File.Exists(configPath))
+                throw new FileNotFoundException($"CrmConfig File not found=>{configPath}");
+
+            var configText = File.ReadAllText(configPath);
+
+            var crmConfigEntity = JsonSerializer.Deserialize<CrmConfig>(configText);
+
+            var crmConnString = string.Format(crmConfigEntity.crmConnRawString,
+                crmConfigEntity.userName,
+                crmConfigEntity.userPwd,
+                crmConfigEntity.crmUrl,
+                crmConfigEntity.crmAppId,
+                crmConfigEntity.redirectUri);
+            return crmConnString;
+        }
+    }
+
+    internal class CrmConfig
+    {
+        public string crmUrl { get; set; }
+        public string crmAppId { get; set; }
+        public string userName { get; set; }
+        public string userPwd { get; set; }
+        public string redirectUri { get; set; }
+        public string crmConnRawString { get; set; }
+    }
+}
