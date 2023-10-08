1b6bfa429a6549c6cbdeb6a969e9c6799c73733b
GaoSong11 <GaoSong11@12XR693>
2023-10-08 13:56:51 +0800

    #4 add Method Retrieve user associated teams

--- ClientApp/Crm.ClientApp.Operations/Crm.ClientApp.Operations/UserOperations/UserSecurityOperation.cs
+++ ClientApp/Crm.ClientApp.Operations/Crm.ClientApp.Operations/UserOperations/UserSecurityOperation.cs
@@ -9,9 +9,15 @@ namespace Crm.ClientApp.Operations.UserOperations
         public void Execute()
         {
             var userId = WhoAmI();
+            RetrieveUserTeams(userId);
             RetrieveUserRoles(userId);
         }
 
+        /// <summary>
+        /// Get security roles for specific user
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
         public EntityCollection RetrieveUserRoles(Guid userId)
         {
             var query = new QueryExpression();
@@ -29,6 +35,37 @@ namespace Crm.ClientApp.Operations.UserOperations
 
             foreach (var roleEntity in result.Entities)
                 Console.WriteLine(roleEntity.GetAttributeValue<string>("name"));
+
+            return result;
+        }
+
+        /// <summary>
+        /// Get teams the user belongs to
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public EntityCollection RetrieveUserTeams(Guid userId)
+        {
+            var query = new QueryExpression();
+            query.EntityName = "team";
+            query.ColumnSet = new ColumnSet("name", "isdefault", "teamtype");
+
+            var linkEntity = new LinkEntity("team", "teammembership", "teamid", "teamid", JoinOperator.Inner);
+            linkEntity.LinkCriteria = new FilterExpression();
+            linkEntity.LinkCriteria.AddCondition("systemuserid", ConditionOperator.Equal, userId);
+            query.LinkEntities.Add(linkEntity);
+
+            var result = crmServiceClient.RetrieveMultiple(query);
+
+            Console.WriteLine($"User with id {userId} is member of  {result.Entities.Count} teams");
+
+            foreach (var entity in result.Entities)
+            {
+                var name = entity.GetAttributeValue<string>("name");
+                var isDefault = entity.FormattedValues["isdefault"];
+                var type = entity.FormattedValues["teamtype"];
+                Console.WriteLine($"Team name=>{name};Is Default=>{isDefault};Team type=>{type}");
+            }
 
             return result;
         }
