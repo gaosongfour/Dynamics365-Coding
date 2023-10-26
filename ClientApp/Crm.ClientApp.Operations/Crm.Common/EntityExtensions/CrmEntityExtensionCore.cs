41ca7711d570968cee4d09e26938026abc7069bc
GaoSong11 <GaoSong11@12XR693>
2023-10-26 14:24:10 +0800

    #9 add FetchXml Aggregation Operations; add new common extension method public static T GetAliasedAttributeValue<T>(this Entity entity, string attributeName)

--- ClientApp/Crm.ClientApp.Operations/Crm.Common/EntityExtensions/CrmEntityExtensionCore.cs
+++ ClientApp/Crm.ClientApp.Operations/Crm.Common/EntityExtensions/CrmEntityExtensionCore.cs
@@ -25,6 +25,24 @@ namespace Crm.Common
             else
                 return null;
         }
+
+        /// <summary>
+        /// Get Crm Entity Aliased Attribute Value to T (string, money, lookup etc), if entity doesn't contain attributeName, return default value of T
+        /// </summary>
+        /// <typeparam name="T">the actual type of attribute that is wrapped by AliasedValue</typeparam>
+        /// <param name="entity"></param>
+        /// <param name="attributeName"></param>
+        /// <returns></returns>
+        public static T GetAliasedAttributeValue<T>(this Entity entity, string attributeName)
+        {
+            if (entity == null) throw new ArgumentNullException("entity can not be null");
+            if (string.IsNullOrEmpty(attributeName)) throw new ArgumentNullException("attributeName can not be null or empty");
+
+            if (!entity.Contains(attributeName)) return default(T);
+            if (entity[attributeName].GetType() != typeof(AliasedValue)) throw new ArgumentException($"attribute {attributeName} is not aliased type");
+
+            return (T)(entity.GetAttributeValue<AliasedValue>(attributeName).Value);
+        }
         #endregion
     }
 }
