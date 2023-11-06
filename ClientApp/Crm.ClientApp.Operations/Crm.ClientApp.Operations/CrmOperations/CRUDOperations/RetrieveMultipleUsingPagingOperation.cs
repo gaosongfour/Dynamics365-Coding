f244317d4f890afe71e8f5992a73e54a66d8f83e
GaoSong11 <GaoSong11@12XR693>
2023-11-06 16:44:09 +0800

    #10 Add Retrieve Multiple Records using Pagination (more than 5000)

---
+++ ClientApp/Crm.ClientApp.Operations/Crm.ClientApp.Operations/CrmOperations/CRUDOperations/RetrieveMultipleUsingPagingOperation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Xrm.Sdk.Query;
+using Crm.Common.Query;
+
+namespace Crm.ClientApp.Operations.CrmOperations
+{
+    public class RetrieveMultipleUsingPagingOperation : CrmOperationBase, ICrmOperation
+    {
+        public void Execute()
+        {
+            RetrieveAllAccounts();
+        }
+
+        private void RetrieveAllAccounts()
+        {
+            var basicQuery = new QueryExpression
+            {
+                EntityName = "account",
+                ColumnSet = new ColumnSet("name"),
+                Criteria = new FilterExpression()
+                {
+                    Conditions =
+                    {
+                        new ConditionExpression("statecode",  ConditionOperator.Equal,0),
+                        new ConditionExpression("name", ConditionOperator.NotNull),
+                    }
+                },
+                Orders = { new OrderExpression("createdon", OrderType.Descending) },
+            };
+
+            var entityList = crmServiceClient.RetrieveAllRecords(basicQuery);
+            Console.WriteLine($"Total accounts=>{entityList.Count}");
+        }
+    }
+}
