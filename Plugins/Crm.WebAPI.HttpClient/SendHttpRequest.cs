68a79870f3daa49c503ee5d6d57b932aa48d6969
GaoSong11 <GaoSong11@12XR693>
2025-01-16 11:48:24 +0800

    #23 create a new Plugin project for Custom API to simulate consuming 3rd WebAPI

---
+++ Plugins/Crm.WebAPI.HttpClient/SendHttpRequest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Net.Http;
+
+namespace Crm.WebAPI.HttpClientDemo
+{
+    public class SendHttpRequest : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            if (!context.InputParameters.Contains("new_crmsendrequest_requesturi"))
+                throw new InvalidPluginExecutionException("missing input parameters new_crmsendrequest_requesturi");
+
+            var uri = context.InputParameters["new_crmsendrequest_requesturi"].ToString();
+            tracingService.Trace($"uri=>{uri}");
+
+            try
+            {
+                var responseText = SendRequest(uri);
+                tracingService.Trace($"response=>{responseText}");
+            }
+            catch (Exception ex)
+            {
+                tracingService.Trace($"ex=>{ex.Message}");
+            }
+        }
+
+        private string SendRequest(string uri)
+        {
+            var client = new HttpClient();
+            client.Timeout = TimeSpan.FromSeconds(60);
+            client.DefaultRequestHeaders.ConnectionClose = true; //Set KeepAlive to false
+
+            var responseBody = client.GetStringAsync(uri).GetAwaiter().GetResult();
+            return responseBody;
+        }
+    }
+}
